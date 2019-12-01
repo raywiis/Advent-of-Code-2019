91c4f2f060393fd928e27f233ce9fa62c34f75bb
RayWiis <raywiis@cock.li>
2019-12-01 17:52:37 +0200

    Add crude solution for first problem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{prelude::*, BufReader, Result};
+use std::cmp;
+
+fn get_fuel_cost(mass: i32) -> i32 {
+    let cargo_cost = cmp::max((mass / 3) - 2, 0);
+    if cargo_cost == 0 {
+        cargo_cost
+    } else {
+        cargo_cost + get_fuel_cost(cargo_cost)
+    }
+}
+
+fn main() -> Result<()> {
+    let file = File::open("res/1_1.txt")?;
+    let reader = BufReader::new(file);
+    let mut fuel_sum = 0;
+
+    for line in reader.lines() {
+        let weight = line?.to_string()
+            .parse::<i32>().expect("Failed to parse");
+        let cost = get_fuel_cost(weight);
+        fuel_sum += cost;
+    }
+
+
+    println!("Total fuel required: {}", fuel_sum);
+    Ok(())
 }
