abb1a741c92ba0e63453a997ebcd062349bfc1f1
RayWiis <raywiis@cock.li>
2019-12-02 22:58:56 +0200

    Refactor day 2 solution

--- src/bin/2.rs
+++ src/bin/2.rs
@@ -4,48 +4,59 @@ use std::fs::File;
 fn main() -> Result<()> {
     let file = File::open("res/2_1.txt")?;
     let reader = BufReader::new(file);
-    let mut memory:Vec<u32> = reader.split(',' as u8)
+    let start_state:Vec<u32> = reader.split(',' as u8)
         .map(|instruction| instruction.unwrap())
         .map(|i| String::from_utf8(i).expect("Failed to parse string"))
         .map(|i| i.parse::<u32>().expect("Failed to parse u8"))
         .collect();
-    let starting_memory = memory.clone();
+
+    let mut memory = start_state.clone();
+
+    memory = run_intcode(memory);
+    println!("[Day 2] memory[0] is {}", memory[0]);
 
     for attempt in 0..99*99 {
+        memory = start_state.clone();
+
         let noun = attempt / 99;
         let verb = attempt % 99;
 
         memory[1] = noun;
         memory[2] = verb;
 
-        let mut idx = 0;
-        while memory[idx] != 99 {
-            let opcode = memory[idx];
-            let pos_a = memory[idx + 1];
-            let pos_b = memory[idx + 2];
-            let target = memory[idx + 3];
+        memory = run_intcode(memory);
 
-            let a = memory[pos_a as usize];
-            let b = memory[pos_b as usize];
-
-            match opcode {
-                1 => std::mem::replace(&mut memory[target as usize], a + b),
-                2 => std::mem::replace(&mut memory[target as usize], a * b),
-                _ => panic!("Unexpected opcode")
-            };
-
-            idx += 4;
-        };
-
-        if memory[0] == 19690720 {
-            let result = 100 * noun + verb;
-            println!("Noun: {}, Verb: {}, Result: {}", noun, verb, result);
-            return Ok(());
+        if memory[0] != 19690720 {
+            continue;
         }
 
-        memory = starting_memory.clone();
+        let result = 100 * noun + verb;
+        println!("[Day 2] noun: {}, verb: {}, result: {}", noun, verb, result);
     }
 
-    println!("After running memory[0] is {}", memory[0]);
     Ok(())
+}
+
+fn run_intcode(mut memory: Vec<u32>) -> Vec<u32> {
+    let mut idx = 0;
+
+    while memory[idx] != 99 {
+        let opcode = memory[idx];
+        let address_a = memory[idx + 1];
+        let address_b = memory[idx + 2];
+        let target = memory[idx + 3];
+
+        let a = memory[address_a as usize];
+        let b = memory[address_b as usize];
+
+        match opcode {
+            1 => std::mem::replace(&mut memory[target as usize], a + b),
+            2 => std::mem::replace(&mut memory[target as usize], a * b),
+            _ => panic!("Unexpected opcode")
+        };
+
+        idx += 4;
+    };
+
+    memory
 }
