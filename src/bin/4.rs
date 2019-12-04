b8f6b7e8158227988b0b075c864c06590bd90585
RayWiis <raywiis@cock.li>
2019-12-04 20:25:49 +0200

    Add solution for day 4

---
+++ src/bin/4.rs
@@ -0,0 +1,47 @@
+
+fn main() {
+    let start = 145852;
+    let end = 616942;
+
+    let mut count = 0;
+    for i in start..end {
+        if verify_password(i) {
+            count += 1;
+        }
+    }
+
+    println!("{} passwords fit", count);
+}
+
+fn verify_password(mut password: i32) -> bool {
+    let mut digits: [i32; 6] = [0; 6];
+    for i in 0..6 {
+        let idx = 5 - i;
+        digits[idx] = password % 10;
+        password /= 10;
+    }
+
+    for i in 1..6 {
+        if digits[i - 1] > digits[i] {
+            return false
+        }
+    }
+
+    let mut group = 1;
+    for i in 1..6 {
+        if digits[i - 1] == digits[i] {
+            group += 1;
+        } else {
+            if group == 2 {
+                return true;
+            } else {
+                group = 1;
+            }
+        }
+    }
+    if group == 2 {
+        return true;
+    }
+
+    false
+}
