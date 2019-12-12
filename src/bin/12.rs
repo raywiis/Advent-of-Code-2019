bb3443d5e9eb71fc13c3d1b3b623902c56675b4c
RayWiis <raywiis@cock.li>
2019-12-12 22:51:10 +0200

    Add solution for day 12 part 2

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,11 +1,10 @@
+extern crate num;
+use num::Integer;
+
 #[derive(Debug, Clone)]
 struct Moon {
     position: (i32, i32, i32),
     velocity: (i32, i32, i32)
-}
-
-fn cmp(a: &Moon, b: &Moon) -> bool {
-    a as *const _ == b as *const _
 }
 
 impl Moon {
@@ -60,35 +59,35 @@ impl Moon {
 }
 
 fn main() {
-    let mut moons: Vec<Moon> = vec!(
+    let initial_moons: Vec<Moon> = vec!(
 
         // Moon::new((-1, 0, 2)),
         // Moon::new((2, -10, -7)),
         // Moon::new((4, -8, 8)),
         // Moon::new((3, 5, -1))
 
-        Moon::new((-8, -10, 0)),
-        Moon::new((5, 5, 10)),
-        Moon::new((2, -7, 3)),
-        Moon::new((9, -8, -3))
+        // Moon::new((-8, -10, 0)),
+        // Moon::new((5, 5, 10)),
+        // Moon::new((2, -7, 3)),
+        // Moon::new((9, -8, -3))
 
-        // Moon::new((-10, -13, 7)),
-        // Moon::new((1, 2, 1)),
-        // Moon::new((-15, -3, 13)),
-        // Moon::new((3, 7, -4))
+        Moon::new((-10, -13, 7)),
+        Moon::new((1, 2, 1)),
+        Moon::new((-15, -3, 13)),
+        Moon::new((3, 7, -4))
 
     );
 
+    let mut moons: Vec<Moon> = initial_moons.clone();
 
     println!("{:?}", moons);
 
     let mut history: Vec<Vec<Moon>> = vec!();
-    let mut repeat = 0;
-
-    for step in 0..100000 {
+    let mut matched_loop_count = 0;
+    let mut loops = [0; 3];
+    for step in 0..4686774924_i64 { 
         let old_moons = moons.clone();
         history.push(moons.clone());
-
         for i in 0..moons.len() {
             moons[i].apply_gravity(&old_moons);
         }
@@ -97,21 +96,46 @@ fn main() {
             moon.apply_velocity();
         }
 
-        for i in 0..history.len() {
-            let time = &history[i];
-            if //time[0].velocity == moons[0].velocity
-                // time[1].velocity == moons[1].velocity
-                // time[2].velocity == moons[2].velocity
-                 time[3].position == moons[3].position
-            {
-                println!("Position match at {}", i);
-                println!("{:?}", time);
-                println!("{:?}", moons);
-                repeat += 1;
-                if repeat == 8 {
-                    return;
-                }
+        if
+        moons[0].position.0 == initial_moons[0].position.0 &&
+        moons[1].position.0 == initial_moons[1].position.0 &&
+        moons[2].position.0 == initial_moons[2].position.0 &&
+        moons[3].position.0 == initial_moons[3].position.0 
+        {
+            if loops[0] == 0 {
+                matched_loop_count += 1;
+                loops[0] = step + 2;
             }
+        }
+
+        if
+        moons[0].position.1 == initial_moons[0].position.1 &&
+        moons[1].position.1 == initial_moons[1].position.1 &&
+        moons[2].position.1 == initial_moons[2].position.1 &&
+        moons[3].position.1 == initial_moons[3].position.1 
+        {
+            if loops[1] == 0 {
+                matched_loop_count += 1;
+                loops[1] = step + 2;
+            }
+        }
+
+        if
+        moons[0].position.2 == initial_moons[0].position.2 &&
+        moons[1].position.2 == initial_moons[1].position.2 &&
+        moons[2].position.2 == initial_moons[2].position.2 &&
+        moons[3].position.2 == initial_moons[3].position.2
+        {
+            if loops[2] == 0 {
+                matched_loop_count += 1;
+                loops[2] = step + 2;
+            }
+        }
+
+        if matched_loop_count == 3 {
+            println!("{:?}", loops);
+            println!("{}", loops[0].lcm(&loops[1]).lcm(&loops[2]));
+            return;
         }
     }
 
