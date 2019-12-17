bee66aac93306b463e0ab549afd80cd4085396d3
RayWiis <raywiis@cock.li>
2019-12-17 22:55:23 +0200

    Add refactor and add tests for day 1

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -1,29 +1,77 @@
 use std::fs::File;
-use std::io::{prelude::*, BufReader, Result};
+use std::io::{prelude::*, BufReader};
 use std::cmp;
 
-fn get_fuel_cost(mass: i32) -> i32 {
+fn main() {
+    let input = read_input("res/1_1.txt");
+    let cargo_fuel = input.iter()
+        .map(|w| get_fuel_cost(*w, false))
+        .sum::<i32>();
+    
+    let total_fuel = input.iter()
+        .map(|w| get_fuel_cost(*w, true))
+        .sum::<i32>();
+
+    println!("[Day 1] Cargo fuel cost: {}", cargo_fuel);
+    println!("[Day 1] Total fuel cost: {}", total_fuel);
+}
+
+fn get_fuel_cost(mass: i32, recurse: bool) -> i32 {
     let cargo_cost = cmp::max((mass / 3) - 2, 0);
-    if cargo_cost == 0 {
+    if cargo_cost == 0 || !recurse {
         cargo_cost
     } else {
-        cargo_cost + get_fuel_cost(cargo_cost)
+        cargo_cost + get_fuel_cost(cargo_cost, true)
     }
 }
 
-fn main() -> Result<()> {
-    let file = File::open("res/1_1.txt")?;
+fn read_input(filename: &str) -> Vec<i32> {
+    let file = File::open(filename)
+        .expect("Failed to open file");
     let reader = BufReader::new(file);
-    let mut fuel_sum = 0;
 
-    for line in reader.lines() {
-        let weight = line?.to_string()
-            .parse::<i32>().expect("Failed to parse");
-        let cost = get_fuel_cost(weight);
-        fuel_sum += cost;
+    let total_weight = reader.lines()
+        .map(|r| r.expect("Failed to read line"))
+        .map(|l| l.to_string())
+        .map(|s| s.parse::<i32>()
+            .expect("Failed to parse int"))
+        .collect();
+    
+    total_weight
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_1_samples() {
+        assert_eq!(get_fuel_cost(12, false), 2);
+        assert_eq!(get_fuel_cost(14, false), 2);
+        assert_eq!(get_fuel_cost(1969, false), 654);
+        assert_eq!(get_fuel_cost(100756, false), 33583);
     }
 
+    #[test]
+    fn part_1() {
+        let fuel_sum = read_input("res/1_1.txt").iter()
+            .map(|w| get_fuel_cost(*w, false))
+            .sum::<i32>();
+        assert_eq!(fuel_sum, 3457281);
+    }
 
-    println!("[Day 1] Total fuel cost: {}", fuel_sum);
-    Ok(())
+    #[test]
+    fn part_2_samples() {
+        assert_eq!(get_fuel_cost(14, true), 2);
+        assert_eq!(get_fuel_cost(1969, true), 966);
+        assert_eq!(get_fuel_cost(100756, true), 50346);
+    }
+
+    #[test]
+    fn part_2() {
+        let fuel_sum = read_input("res/1_1.txt").iter()
+            .map(|w| get_fuel_cost(*w, true))
+            .sum::<i32>();
+        assert_eq!(fuel_sum, 5183030);
+    }
 }
